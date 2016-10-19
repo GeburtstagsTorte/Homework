b49d12095c3d8d44b40caec1d4c5024dea8d7df3
Simon Sacher <simonsacher@gmail.com>
2016-10-19 18:40:49 +0200

    2_1 finished

---
+++ Homework Solution/homework2_1.py
@@ -0,0 +1,25 @@
+def take_input():
+    x = input("Please type a number: ")
+    try:
+        x = int(x)
+    except ValueError:
+        pass
+
+    return x
+
+
+def return_list(x):
+    lst = []
+    for i in range(1, x+1):
+        for j in range(i):
+            lst.append(i)
+    return lst[:x]
+
+
+def main():
+    u = take_input()
+    print(return_list(u))
+
+
+if __name__ == "__main__":
+    main()
