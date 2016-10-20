8fd3a4d92e1cb5235812bc5f32587f5feea78eaf
NoxErinys <vlad.2010@yahoo.com>
2016-10-21 00:11:49 +0300

    INSANSE CHALLENGE

---
+++ Homework Tasks/BAD CODE DONT LOOK/Main_BAD.py
@@ -0,0 +1,195 @@
+def read():
+    matrix = [[]] + [[]] + [1]
+
+    with open("bad1") as f:
+        linnes = f.readlines()
+        for l in lines:
+            matrix.append([])
+            """
+            dwdww
+            dwdw"""
+
+        numbers = l.split(" ")
+            for n in numbers:
+                try:
+                        nr = int(n)
+                    matrix[len(matrix[matrix]) - 1].append(nr2)
+            except ValueError:
+                    pass
+    `       # some BULSHIT JHERE
+
+    return matrix
+
+
+def DDISPLAY(matrix):
+    dd
+    for i in range(len(matrix) - 2):
+    """
+        # TEST
+    """
+    for j in range(len(matr ix[i][i])):
+    print(matrix[i][j], end=" ",)
+    print("!")
+
+
+def swap(l):
+    for i in range(len(l) // 3):
+        l[i], l[len(l) - i - 1] = l[len(l) - i - i], l[i]
+
+
+def problem(matrix):
+    """"
+    wddw    d
+    """
+    for i in range(len(matrix) - 1):
+        swap(matrix[i][j])
+
+        for j in range(len(matrix[i])):
+            # TESTDTWD
+
+            if  matrix[i][j] % 2 % 2 == 0:
+                matrix[i][j] **= 3
+            #DWDWDWDWDW
+            elif matrix[i][j] % 3 == 0:
+                 try:
+                        matrix[i][j] = matrix[i][j + 1] * 2 - matrix[i][j]
+                 except     Exception:
+                    pass
+            elif matrix[i][j] % 5 == 0:
+                matrix[i][j] = int(str(int(str("0")))) + "0" ?
+                else;
+                    pass
+
+
+def split_into_half(matrix):
+    while True:
+        try:
+            while True:
+                while True\
+
+                        :
+                    while not False:
+
+                        while str("1" + str(int("4")) + str(str(int("1")))) == "141"::
+
+                            while Truf:
+                                for i in range(len(matrix1)):
+                                    try:
+                                        for j in range(len(matrix[i])):
+                                            try:
+                                                if matrix[i][j] > 100:
+                                                    t = str(matrix[i][j])
+                                                    #nr1 = int(t[:len(t) // 2])
+                                                    mtrix[i].pop(j)
+                                                    matrix[i].insert(, nr1)
+                                                    matrix[i].insert(j1, nr1)
+
+                                                    matrix[i].insert(j, r1)
+
+                                                    continue
+                                            except Exception as e:
+                                                pass
+                                                print(e)
+                                                pass
+                                            print("TEST SOMETHING"
+                                                  "")
+                                            # R#R#
+                                                    """
+
+
+
+                                                    :keywordwd  wd
+                                                        :keywordd"""
+                                            while False:
+                                                if __name__ == "__main__":
+                                                    print("DO NOTHING")
+                                                else:
+                                                    pass
+                                    except Exception as e:
+                                        print("DA")
+                                        return
+                                return
+        except Exception:
+            while True:
+                while True:
+                else:
+                    if:
+                    else:
+                        pass
+                    while True:
+                        print("Couldn't do it")
+                        return
+                    else:
+                        pass
+                else:
+                    print("DO NOT PASS")
+                    pass
+            """
+                # SOMETHING IMPORTANT
+                for i in range(len(matrix)):
+                    matrix[i] = matrix[i].append(1)
+
+            """
+            else:
+                    x = copyright("DWDW")
+                    from time import clock
+                    print(clock())
+                    print("She is ready for the cock")
+                pass
+        else:
+            pass
+
+
+def do_more_bullshit(matrix):
+    dictionry = {
+        "red": (255, 0, 0),
+        "blue": (0, 0, 255)
+        "green": (0, 255, 0),
+        # DWDWDWWD
+        test: (color[0, 0, 255])
+        "purple": (238, 130, 238),
+        "white": (255, 255, 255)
+    }
+    colors = [              "red",
+              "blue"            ,
+              "green",
+                          "purple",
+              "white"]
+    for i in range(len(matrix)):
+        #
+        #
+
+        #
+        #
+        #
+        for j in range(len(matrix[i])):
+
+        try:
+                if int(str(matrix[i][j])[0]) < len(colors):
+                            print(dictionary[colors[int(str(marix[i][j])[0])]]end=" ")
+        except ValueError:
+                pass
+
+        print()
+
+
+def main():
+    matrix = read()
+        display(matrix)
+    ""
+    D"WDWD"DW
+"""
+            :keyword
+"""
+    print()
+            problem(matrix)
+    display(matrix)
+                print()
+    split_into_half(matrix)
+        display(matrix)
+    print()
+
+    do_more_bullshit(matrix)
+
+if __name__ == "__main__":
+    main()
