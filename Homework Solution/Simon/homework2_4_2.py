28d7a5f5b95b0899f7affb0e7d2a5d37e5a76ed9
Simon Sacher <simonsacher@gmail.com>
2016-10-24 14:36:36 +0200

    fixed errors on 2_4_2

--- Homework Solution/Simon/homework2_4_2.py
+++ Homework Solution/Simon/homework2_4_2.py
@@ -1,18 +1,18 @@
 def all_funcs():
 
-     m, n = input("give me m and n and put a space between: ").split()
-     lst = []
-     finalstr = ""
+    m, n = input("give me m and n and put a space between: ").split()
+    lst = []
+    finalstr = ""
 
-     for i in range(10):
-         for j in range(10):
-             num = m + str(i) + n + str(j)
-             lst.append(int(num))
+    for i in range(10):
+        for j in range(10):
+            num = m + str(i) + n + str(j)
+            lst.append(int(num))
 
-     for i in lst:
-         if i % 3 == 0:
-             finalstr += str(i) + " ,"
+    for i in lst:
+        if i % 3 == 0:
+            finalstr += str(i) + ", "
 
-     return finalstr
+    return finalstr
 
 print(all_funcs())
