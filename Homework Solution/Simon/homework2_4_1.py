d425e7c25f1d8c5e1eb71a2cd125b1115ab4c0a6
Simon Sacher <simonsacher@gmail.com>
2016-10-24 13:15:52 +0200

    homework 2_4_1 added/solved

---
+++ Homework Solution/Simon/homework2_4_1.py
@@ -0,0 +1,51 @@
+def take_input():
+
+    m, n = input("give me m, n with a space between").split()
+    try:
+        m, n = int(m), int(n)
+        return str(m), str(n)
+    except ValueError:
+        pass
+
+
+def take_string(m, n):
+
+    num = ""
+    nums = []
+
+    try:
+        for i in range(10):
+            for j in range(10):
+                num += m + str(i) + n + str(j)
+                if manipulate(num):
+                    nums.append(num)
+                num = ""     # haha that took me a minute lol (but i print debugged ;))
+    except ValueError:
+        pass
+
+    return nums
+
+
+def manipulate(num):  # find name
+
+    try:
+        num = int(num)
+        if num % 3 == 0:
+            return num
+    except ValueError:
+        pass
+
+    # bad approach
+    """for i in lst:
+        if i % 3 == 0:
+            lst.append(i)
+
+    return lst2"""
+
+
+def main():
+    m, n = take_input()
+    print(take_string(m, n))
+
+if __name__ == "__main__":
+    main()
