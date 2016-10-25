497849b0afc043546db1a9553184a98c79f0e9ec
Korogi <samuelx5yahoo.de>
2016-10-25 20:38:49 +0200

    Test1 (my solution)

---
+++ Homework Solution/Samuel/TEST 1.py
@@ -0,0 +1,31 @@
+#TEST 1
+
+
+def take_input():
+    with open("test1 file", "r") as f:
+        s = f.read()
+        lst = [int(x) for x in s.split(", ") if x.isdigit()]
+
+        return lst
+
+
+def is_permutation(lst):
+    count = 0
+    sum_lst = ((len(lst)*(len(lst)+1))/2)
+    
+    for x in lst:
+        count += x
+
+    if count == sum_lst:
+        return True
+    else:
+        return False
+
+
+def main():
+    u = take_input()
+    print(is_permutation(u))
+
+
+if __name__ == "__main__":
+    main()
