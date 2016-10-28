1780580a371c21ce64fe0b5543854cdcf819afee
NoxErinys <vlad.2010@yahoo.com>
2016-10-28 22:19:36 +0300

    Matrix_1

---
+++ Homework Solution/Vlad/matrix1.py
@@ -0,0 +1,6 @@
+m = open("matrix1").read().splitlines()
+m = [[int(i) for i in j.split()] for j in m]
+print("First quarter: ", [m[i][j] for i in range(len(m)//2) for j in range(len(m) // 2)])
+print("Second quarter: ", [m[i][j] for i in range(len(m)//2) for j in range(len(m) // 2, len(m))])
+print("Third quarter: ", [m[i][j] for i in range(len(m)//2, len(m)) for j in range(len(m) // 2)])
+print("Fourth quarter: ", [m[i][j] for i in range(len(m)//2, len(m)) for j in range(len(m) // 2, len(m))])
