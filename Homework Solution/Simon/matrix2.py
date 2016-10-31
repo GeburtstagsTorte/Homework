57c76564a792ea229b1d701d045c9df7ea688c2b
NoxErinys <vlad.2010@yahoo.com>
2016-10-31 17:08:40 +0200

    Fixed Simon's code

--- Homework Solution/Simon/matrix2.py
+++ Homework Solution/Simon/matrix2.py
@@ -6,39 +6,27 @@ def open_matrix():
     return m
 
 
-def append_at_lists_column(m):
+def append_at_lines(m):
 
     for i in m:
-
         if eval('*'.join(str(k) for k in i)) > 0:
             i.append(1)
 
         if eval('*'.join(str(j) for j in i)) < 0:
             i.append(-1)
 
-    return m
 
+def append_at_columns(m):
 
-def append_a_list(m):
+    new_column = []
 
-    lst = []
-    lst2 = []
-    for i in range(len(m)):              # for i in range(len(m)): # for j in range(i, (i+1)):
+    for i in range(len(m[0])):
+        p = 1
         for j in range(len(m)):
-            print(j)
-            print(i)
-            lst.append(m[j][i])
-            print("lst", lst)
+            p *= m[j][i]
+        new_column.append(p)
 
-        if eval('*'.join(str(k) for k in lst)) > 0:
-            lst2.append(1)
-
-        if eval('*'.join(str(l) for l in lst)) < 0:
-            lst2.append(-1)
-
-    print("lst2: ", lst2)
-    m.append(lst2)
-    return m
+    m.append(new_column)
 
 
 def print_matrix(m):
@@ -55,10 +43,9 @@ def print_matrix(m):
 
 def main():
     m = open_matrix()
-    n = append_at_lists_column(m)
-    o = append_a_list(n)
-    print_matrix(o)
-    #print_matrix(n)
+    append_at_lines(m)
+    append_at_columns(m)
+    print_matrix(m)
 
 if __name__ == '__main__':
     main()
