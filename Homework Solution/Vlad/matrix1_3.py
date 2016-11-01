abccff2cec9917bf59c7ad365b88381eba38791d
NoxErinys <vlad.2010@yahoo.com>
2016-11-01 22:01:02 +0200

    Matrix1_3

--- Homework Solution/Vlad/matrix1_3.py
+++ Homework Solution/Vlad/matrix1_3.py
@@ -1,4 +1,4 @@
 m = open("matrix3").read().splitlines()
-m = [[int(i) for i in j.split()] for j in m]
-m = [[m[i][j] for j in range(len(m[0]))] for i in range(len(m))]
-print(''.join([str(m[i]) + "{0}" for i in range(len(m))]).format("\n"))
+m = [[int(i) for i in j.split()] + [-1] for j in m]
+m = [[[m[i][j], int(['', str(m[i][j-1])][j-1 >= 0] + str(m[i][j]) + ['', str(m[i][j+1])][j+1 < len(m[0])-1])][(i+j)%2 != 0] for j in range(len(m[0])-1)] for i in range(len(m))]
+print(''.join([str([m[i][j] for j in range(len(m[0]))]) + "{0}" for i in range(len(m))]).format("\n"))
