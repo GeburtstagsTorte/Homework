36b39f4a11a45626144d70cc64d0e1cd396197ca
Simon Sacher <simonsacher@gmail.com>
2017-04-20 22:08:13 +0200

    strucc

---
+++ Samuels Projects/main_pygame.py
@@ -0,0 +1,62 @@
+import pygame
+from graph_pygame import Graph
+
+
+class Game:
+    clock = pygame.time.Clock()
+    game_exit = False
+
+    def __init__(self, title, width, height, background_color=(255, 255, 255)):
+        self.title = title
+        self.width = width
+        self.height = height
+        self.background_color = background_color
+
+        self.game_display = pygame.display.set_mode((width, height), pygame.SRCALPHA)
+        pygame.display.set_caption(title)
+        self.main_loop()
+
+    def main_loop(self):
+        while not self.game_exit:
+
+            for event in pygame.event.get():
+                self.handle_keys(event)
+
+            self.game_display.fill(self.background_color)
+            self.render(self.game_display)
+            pygame.display.update()
+            self.update()
+            self.clock.tick(60)
+
+    @staticmethod
+    def render(game_display):
+        Game.render_graph(game_display, (0, 0, 0), (100, 100), (100, 600), (701, 600))
+        # Entities.render(game_display)
+
+    @staticmethod
+    def update():
+        # Entities.update()
+        pass
+
+    def handle_keys(self, event):
+        if event.type == pygame.QUIT:
+            self.game_exit = True
+
+    @staticmethod
+    def render_graph(surface, color, start_pos, mid_pos, end_pos):
+        Graph(surface, color, start_pos, mid_pos, end_pos)
+        Graph.draw_graph(surface, color, start_pos, mid_pos, end_pos)
+
+
+def main():
+    pygame.init()
+
+    width = 1100
+    height = 700
+    Game("graphs", width, height)
+
+    pygame.quit()
+    quit()
+
+if __name__ == '__main__':
+    main()
