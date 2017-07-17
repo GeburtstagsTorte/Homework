336ec25477890e4130c04b1283b069f11fb4ff31
Simon Sacher <simonsacher@gmail.com>
2017-07-17 20:50:58 +0200

    del redundancies

--- Projects/php_simon/captchas_17.07/captchas_17.07/captcha_frameset/frameset.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html><html>
-<frameset rows="*,2*">
-	<p><frame src="create_captcha_session.php" name ="eingabe"></p>
-	<p><frame src="check_input.php" name="ausgabe"></p>
-</frameset>
-</html>
