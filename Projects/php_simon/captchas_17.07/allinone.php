459d2ddc76f827e9a33cc0edbda6bb658b1bd7f0
Simon Sacher <simonsacher@gmail.com>
2017-07-17 15:40:45 +0200

    captchas 17.07

---
+++ Projects/php_simon/captchas_17.07/allinone.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html><html><head><meta charset = "UTF-8"></head>
+<body>
+<form action = "allinone.php" method = "post">
+<?php
+if (isset($_POST['gesendet'])){
+    
+    function checkInput(){
+        
+        $pdo2 = new PDO('mysql:host=localhost;dbname=testdatenbank', 'root', '');
+        $sql = "SELECT code FROM session WHERE id = 0";
+        $correct_code = $pdo2->query($sql)->fetch();
+        $eingabe = doubleval($_POST["eingabe"]);
+        
+        if ($eingabe != $correct_code['code']){
+            echo "<br>put the captcha in! are you a robot?<br> try again!<br>";
+            // include 'create_captcha_session.php';
+        }
+        if ($eingabe == $correct_code['code']){
+            echo "the correct code was " .$correct_code['code'];
+            echo "<br>you are not a robot";
+        }
+    }
+    checkInput();
+}
+
+function randomizeColors($c0=50,$c1=50,$c2=50){
+    $c=[$c0, $c1, $c2];
+    $i = rand(0, 2);
+    $c[$i]=255;
+    return $c;
+}
+
+function createImage($cl_r, $cl_g, $cl_b, $code, $x=250, $y=75){
+    $image = imagecreate($x, $y);
+    $farbe = imagecolorallocate($image, $cl_r, $cl_g, $cl_b);
+    imagefill($image, $x, $y, $farbe);
+    
+    $fontFarbe = imagecolorallocate($image, $cl_b, $cl_r, $cl_g);
+    $schriftart = "C:\Windows\Fonts\impact.ttf";
+    $ri = rand(25, -25);
+    $ri2 = rand(2, 4);
+    
+    imagettftext($image, rand(15, 25), $ri, $x/$ri2-$ri/4, $y/2+$ri/4, $fontFarbe, $schriftart, $code);
+    
+    imagejpeg($image, "current_img.jpeg");
+    imagedestroy($image);
+}
+
+session_start();
+$sessionid = session_id();
+$code = rand(1000, 9999);
+
+$pdo = new PDO('mysql:host=localhost;dbname=testdatenbank', 'root', '');
+$statement = $pdo->prepare('UPDATE session SET sessionid = :sessionid, code = :code WHERE id = 0');
+$statement->execute(array('sessionid'=>$sessionid, 'code'=>$code));
+
+$c = randomizeColors();
+createImage($c[0], $c[1], $c[2], $code);
+?>
+<p><img src="current_img.jpeg"></p>
+<p><input name="eingabe" maxlength="4"> CAPTCHA Eingabe</p>
+<p><input type="submit" name="gesendet"> <input type="reset"></p>
+</form>
+</body></html>
---
+++ Projects/php_simon/captchas_17.07/captchas_17.07/captcha_frameset/frameset.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html><html>
+<frameset rows="*,2*">
+	<p><frame src="create_captcha_session.php" name ="eingabe"></p>
+	<p><frame src="check_input.php" name="ausgabe"></p>
+</frameset>
+</html>
