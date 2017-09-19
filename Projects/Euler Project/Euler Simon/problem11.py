de6e189a8196310ac26e65c274e64ae47a73c4ad
Simon Sacher <simonsacher@gmail.com>
2017-09-19 20:33:59 +0200

    fix

--- Projects/Euler Project/Euler Simon/problem11.py
+++ Projects/Euler Project/Euler Simon/problem11.py
@@ -35,24 +35,25 @@ def get_diagonal_right(grid, i, j):
         return 0
 
 
-def main(grid, i, j, temp_product, t1):
-    temp = temp_product
-    if i == 20:
-        t2 = time()
-        exit("Biggest Product: " + str(temp) + "\nTime: " + str((t2 - t1)*1000))
-
+def main(grid, i, j, temp_product):
     for product in [get_x(grid, i, j), get_y(grid, i, j), get_diagonal_left(grid, i, j),
                     get_diagonal_right(grid, i, j)]:
-        if product > temp:
-            temp = product
+        if product > temp_product:
+            temp_product = product
 
     if j == 19:
         i += 1
         j = -1
 
-    main(grid, i, j+1, temp, t1)
+    if i < 20:
+        return main(grid, i, j+1, temp_product)
+    else:
+        return temp_product
 
 
 if __name__ == '__main__':
-    t = time()
-    main(get_grid(), 0, 0, 0, t)
+    t1 = time()
+    biggest_prod = main(get_grid(), 0, 0, 0)
+    print(biggest_prod)
+    t2 = time()
+    exit("Biggest Product: " + str(biggest_prod) + "\nTime: " + str((t2 - t1) * 1000))
