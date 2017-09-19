2bfac77fb4454e558d650c5ce2e317fa6063c3c6
Simon Sacher <simonsacher@gmail.com>
2017-09-19 20:53:44 +0200

    some really minor stuff

--- Projects/Euler Project/Euler Simon/problem2.py
+++ Projects/Euler Project/Euler Simon/problem2.py
@@ -10,17 +10,15 @@ def fibonacci_to_max_value(n):
             return lst[:len(lst)-1]
 
 
-def main(lst):
+def main(input_list):
     value = 0
-    for i in lst[1::3]:
+    for i in input_list[1::3]:
         value += i
     return value
 
 
 if __name__ == '__main__':
     t1 = time.time()
-    lst = fibonacci_to_max_value(4000000)
-    result = main(lst)
+    result = main(fibonacci_to_max_value(4000000))
     t2 = time.time()
-    print(result)
-    print((t2-t1)*1000)
+    exit("Result: " + str(result) + "\nTime: " + str((t2 - t1) * 1000))
