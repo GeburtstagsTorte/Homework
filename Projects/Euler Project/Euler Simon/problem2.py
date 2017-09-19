7eff4b0da4909fc328dba8cf1e9a1a2816ca5177
Simon Sacher <simonsacher@gmail.com>
2017-09-19 20:49:42 +0200

    some really minor fixes

--- Projects/Euler Project/Euler Simon/problem2.py
+++ Projects/Euler Project/Euler Simon/problem2.py
@@ -1,10 +1,9 @@
-import math
 import time
 
 
 def fibonacci_to_max_value(n):
     lst = [1, 2]
-    for i in range(int(math.sqrt(n))):
+    for i in range(n//2):
         if lst[len(lst)-1] < n:
             lst.append(lst[len(lst)-2] + lst[len(lst)-1])
         else:
@@ -17,8 +16,11 @@ def main(lst):
         value += i
     return value
 
+
 if __name__ == '__main__':
     t1 = time.time()
     lst = fibonacci_to_max_value(4000000)
-    print(main(lst))
-    print((time.time()-t1)*1000)
+    result = main(lst)
+    t2 = time.time()
+    print(result)
+    print((t2-t1)*1000)
