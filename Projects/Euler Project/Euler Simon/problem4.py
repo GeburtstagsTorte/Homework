7f330ed7cfb1089ce732c4f4b2ebeab55f1cfcda
Simon Sacher <simonsacher@gmail.com>
2017-09-26 10:37:20 +0200

    problem 4

---
+++ Projects/Euler Project/Euler Simon/problem4.py
@@ -0,0 +1,21 @@
+import time
+
+
+def main():
+    largest = 0
+    for i in range(999, 100, -1):
+        for j in range(999, 100, -1):
+            string = str(i*j)
+            if len(string) < 6:
+                return largest
+            if string[:len(string)//2] == string[len(string):len(string)//2-1:-1]:
+                competitor = int(string)
+                if competitor > largest:
+                    largest = competitor
+
+
+if __name__ == '__main__':
+    t1 = time.time()
+    result = main()
+    t2 = time.time()
+    exit("Result: " + str(result) + "\nTime: " + str((t2 - t1) * 1000))
