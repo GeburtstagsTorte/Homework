bfb5c3ca9f58975df518dcb687d33f36ae3217e5
Simon Sacher <simonsacher@gmail.com>
2017-09-28 12:33:20 +0200

    updated problem7

--- Projects/Euler Project/Euler Simon/problem7.py
+++ Projects/Euler Project/Euler Simon/problem7.py
@@ -10,9 +10,23 @@ def n_th_prime(n):
                 return lst[-1]
 
 
+def n_th_prime_v2(n):
+    count = 1
+    contender = 3
+    while count < n-1:
+        contender += 2
+        if any(contender % i == 0 for i in range(3, int(contender**0.5)+1, 2)):
+            continue
+        count += 1
+    return contender
+
+
 if __name__ == '__main__':
     from time import time
+
     t1 = time()
-    result = n_th_prime(10001)
+    result = n_th_prime_v2(10001)
     t2 = time()
     exit("Result: " + str(result) + "\nTime: " + str((t2 - t1) * 1000))
+
+
